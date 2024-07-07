692ae93bb942506b280725467a97adda8460b4d5
GHULAM MUSTAFA <161878014+Mustafa-Khan786@users.noreply.github.com>
2024-07-07 11:01:23 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,33 +1,59 @@
 
-import inquirer from "inquirer";
-const answers =await inquirer.prompt([
-{
-    type:"number",
-    name:"numone",
-    message: "kindly inter your first number",
+import inquirer from "inquirer"
+import chalk from "chalk";
 
-}, 
-{type:"number",
-    name:"numtwo",
-     message: "kindly inter your second number",
 
-},
-{
-    type: "list",
-    name:"opperator",
-    choices: ['addition','subtraction','multiplication',"division"],
-    message:"Sellect your opperator:"
-}
-]) 
- if (answers.opperator ==="addition") {
-    console.log(answers.numone + answers.numtwo)   
-}
-if (answers.opperator ==="subtraction") {
-    console.log(answers.numone - answers.numtwo)  
-}
-if (answers.opperator ==="multiplication") {
-    console.log(answers.numone * answers.numtwo)
-}
-if (answers.opperator ==="division") {
-            console.log(answers.numone / answers.numtwo)   
+
+let Convertion = {
+    "PKR": {
+      "USD": 0.0036101083032491,
+      "GBP": 0.0037,
+      "PKR": 1
+    },
+    "GBP": {
+      "USD": 1.21,
+      "PKR": 271.79,
+      "GBP": 1
+    },
+    "USD": {
+      "PKR": 277,
+      "GBP": 0.83,
+      "USD": 1
     }
+  }
+  const answer: {
+    from: "PKR"| "USD" | "GBP",
+    to: "PKR"| "USD" | "GBP",
+    amount: number
+  } = await inquirer.prompt([
+    {
+        type: "list",
+        name: "from",
+        choices: ["PKR", "USD", "GBP"],
+        message: "Select your currency: "
+    },
+    {
+        type: "list",
+        name: "to",
+        choices: ["PKR", "USD", "GBP"],
+        message: "Select your convertion currency: "
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter your convertion amount: "
+    }
+  ]);
+
+  const {from, to, amount} = answer;
+
+  if(from && to && amount) {
+    let result = Convertion[from][to] * amount;
+    console.log(`Your convertion from ${chalk.greenBright.bold(from)} to ${chalk.greenBright.bold(to)}
+    
+    is ${chalk.greenBright.bold(result)}`)
+  } else {
+    console.log(chalk.green("Invalid inputs")
+    
+    )
+  }
